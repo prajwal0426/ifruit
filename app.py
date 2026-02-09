701841188daf53471fd098e159ff848aa144ef39
prajwal thalathoti <prajwalthalathoti@gmail.com>
2026-02-09 21:50:22 +0530

    py.app is now regester and running on http://

--- app.py
+++ app.py
@@ -2,21 +2,36 @@ from flask import Flask, render_template, request, redirect, url_for
 
 app = Flask(__name__)
 
-PASSWORD = "123"
+# Temporary user storage (username: password)
+users = {}
 
 @app.route("/")
 def index():
     return render_template("index.html")
+
+@app.route("/register", methods=["POST"])
+def register():
+    username = request.form.get("username")
+    password = request.form.get("password")
+
+    if not username or not password:
+        return render_template("index.html", error="All fields are required")
+
+    if username in users:
+        return render_template("index.html", error="User already exists")
+
+    users[username] = password
+    return render_template("index.html", success="Registration successful! Please login.")
 
 @app.route("/login", methods=["POST"])
 def login():
     username = request.form.get("username")
     password = request.form.get("password")
 
-    if password == PASSWORD and username:
+    if username in users and users[username] == password:
         return redirect(url_for("home", user=username))
     else:
-        return render_template("index.html", error="Wrong password")
+        return render_template("index.html", error="Invalid username or password")
 
 @app.route("/home")
 def home():
