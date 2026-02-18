7bfa8c8e64b3f8ee5d846eea339975db2ee3783a
prajwal thalathoti <prajwalthalathoti@gmail.com>
2026-02-18 21:43:47 +0530

    updated with new kwys

--- app.py
+++ app.py
@@ -32,11 +32,10 @@ google = oauth.register(
     name="google",
     client_id=os.getenv("GOOGLE_CLIENT_ID"),
     client_secret=os.getenv("GOOGLE_CLIENT_SECRET"),
-    access_token_url="https://oauth2.googleapis.com/token",
-    authorize_url="https://accounts.google.com/o/oauth2/auth",
-    api_base_url="https://www.googleapis.com/oauth2/v2/",
+    server_metadata_url="https://accounts.google.com/.well-known/openid-configuration",
     client_kwargs={"scope": "openid email profile"},
 )
+
 
 # -------------------------------------------------
 # Routes
