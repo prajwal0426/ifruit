bedcb5971d92695c65c73acc3cfefbc79064768d
prajwal thalathoti <prajwalthalathoti@gmail.com>
2026-02-27 22:13:00 +0530

    port changed to goblal server

--- app.py
+++ app.py
@@ -316,5 +316,5 @@ def logout():
 # Run app
 # -------------------------------------------------
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 5000))
+    port = int(os.environ.get("PORT", 10000))
     app.run(host="0.0.0.0", port=port, debug=True)
